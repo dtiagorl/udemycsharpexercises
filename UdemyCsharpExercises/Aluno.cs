7c311a56a2130a15ea148e87dab34ecd4ffae064
Tiago <dtiagorl@gmail.com>
2019-03-19 15:58:41 -0300

    Added the Aluno class

---
+++ UdemyCsharpExercises/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyCsharpExercises
+{
+    class Aluno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        public Aluno()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Id + "Name: " + Name + "age of " + Age + "Email: " + Email;
+        }
+    }
+}
